57b3a4aa2dc6aba8941e4803787cba57a96a1914
be4ok <47221680+be4ok@users.noreply.github.com>
2019-03-10 21:35:15 +0200

    initial commit

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -123,6 +123,9 @@ module.exports = function(webpackEnv) {
         ? 'inline-source-map'
         : false
       : isEnvDevelopment && 'cheap-module-source-map',
+      devServer: {
+          disableHostCheck: true
+      },
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
     entry: [
@@ -302,7 +305,7 @@ module.exports = function(webpackEnv) {
               options: {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
@@ -335,7 +338,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -375,7 +378,7 @@ module.exports = function(webpackEnv) {
                 ],
                 cacheDirectory: true,
                 cacheCompression: isEnvProduction,
-                
+
                 // If an error happens in a package, it's possible to be
                 // because it was compiled. Thus, we don't want the browser
                 // debugger to show the original code. Instead, the code
