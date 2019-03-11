30b05df081f06357503014aed2aedb6af8e11e1f
be4ok <47221680+be4ok@users.noreply.github.com>
2019-03-11 21:48:40 +0200

    Authentication error handle

--- src/proxy.js
+++ src/proxy.js
@@ -1,2 +1,2 @@
-//export const PROXY_LINK = "https://taskboard-39.herokuapp.com";
-export const PROXY_LINK = "http://localhost:8080";
+export const PROXY_LINK = "https://taskboard-39.herokuapp.com";
+//export const PROXY_LINK = "http://localhost:8080";
