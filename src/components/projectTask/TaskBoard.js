57a8f6964ffa20f0e3f951c9c8f99caa0d08698c
be4ok <47221680+be4ok@users.noreply.github.com>
2019-05-02 17:52:45 +0200

    Work on design

--- src/components/projectTask/TaskBoard.js
+++ src/components/projectTask/TaskBoard.js
@@ -120,7 +120,7 @@ class TaskBoard extends Component {
         const taskBoard = <React.Fragment>
             <div className="row">
                 <div className="col-md-4">
-                    <div className="card text-center mb-2">
+                    <div className="card text-center">
                         <div className="card-header bg-secondary text-white">
                             <h3>TO DO</h3>
                         </div>
@@ -133,7 +133,7 @@ class TaskBoard extends Component {
 
                 </div>
                 <div className="col-md-4">
-                    <div className="card text-center mb-2">
+                    <div className="card text-center">
                         <div className="card-header bg-primary text-white">
                             <h3>In Progress</h3>
                         </div>
@@ -146,7 +146,7 @@ class TaskBoard extends Component {
 
                 </div>
                 <div className="col-md-4">
-                    <div className="card text-center mb-2">
+                    <div className="card text-center">
                         <div className="card-header bg-success text-white">
                             <h3>Done</h3>
                         </div>
